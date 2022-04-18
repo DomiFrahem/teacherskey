6857a6651cda13c754f0bfe55bbbcbd0ce0d62b9
Alex Sidorov <38810408+DomiFrahem@users.noreply.github.com>
2022-04-18 18:06:20 +0300

    Add files via upload
    Добавлены новы позиции в словарь

--- teacherskey/lang/en/enrol_teacherskey.php
+++ teacherskey/lang/en/enrol_teacherskey.php
@@ -41,3 +41,4 @@ $string['error_update'] = 'Fail update';
 $string['canntenrol'] = 'Enrolment is disabled or inactive';
 $string['defaultrole'] = 'Default role assignment';
 $string['defaultrole_desc'] = 'Select role which should be assigned to users during self enrolment';
+$string['error_teachers_checkbox'] = 'Check if the name of the teacher is correct, and check the box';
