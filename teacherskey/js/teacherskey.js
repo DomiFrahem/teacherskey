cdcea9a19111c1e99e2f2717ecec002888a37c15
Alex Sidorov <38810408+DomiFrahem@users.noreply.github.com>
2022-04-18 18:05:11 +0300

    Delete teacherskey/js directory
    Больше не нужно, заменил на стандартные способы

--- teacherskey/js/teacherskey.js
+++
@@ -1,7 +0,0 @@
-function change_readonly(data) {
-    if (!data.checked){
-        document.getElementById('id_fio').removeAttribute('readonly')
-    }else{
-        document.getElementById('id_fio').setAttribute('readonly', true)
-    }
-}
